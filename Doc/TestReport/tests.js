a724ff17f53aaacee0918c27be163b366ecb4b4d
Josh Mitchell <mitchjp3@mcmaster.ca>
2016-12-06 19:09:45 -0500

    Add Test Report Template

--- Doc/TestReport/tests.js
+++ Doc/TestReport/tests.js
@@ -1,3 +1,8 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "deepEqual test", function( assert ) {
+
+  assert.deepEqual( getURL(), pageURL, "URLs must match" );
+});
